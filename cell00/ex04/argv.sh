f467d1933c6f4546574a2f075d3df0cf1057d279
rnoonson <68100451@kmitl.ac.th>
2025-09-21 09:21:47 +0000

    ex04

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,13 @@
+#!/bin/blash
+
+if [$# -eq 0]; then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+for i in{1...3};do
+    if [$i -le $#]; then
+        eval"arg=\$$i"
+        echo"$arg"
+    fi
+done
